0f8aeceb8533152c2082ce0e8c70b756f8b958ec
adnan <cigtekinadnan@gmail.com>
2019-11-01 01:47:21 +0300

    added an example for replacing images on a website

---
+++ examples/replacing-images-with-berqai/content.js
@@ -0,0 +1,4 @@
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = "https://raw.githubusercontent.com/bridge-software/AdMemer/master/placeholders/placeholder.jpeg"
+}
